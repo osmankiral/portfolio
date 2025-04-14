38330e685c9c3e2747fd1b090675d6b93f2f1612
osmankiral <osman.kiral96@gmail.com>
2025-04-14 09:24:57 +0300

    SEO optimization has been completed.

---
+++ lib/siteMetaData.tsx
@@ -0,0 +1,17 @@
+export const siteMetadata = {
+    title: 'Osman Kıral | Full-Stack Web Developer',
+    author: 'Osman Kıral',
+    headerTitle: 'Next.js Portfolio',
+    description: "Osman Kıral'ın resmi portföyü. React, Next.js, TypeScript, Node.js ve modern web teknolojileriyle geliştirdiği projeleri keşfedin.",
+    language: 'en-us',
+    theme: 'system', // system, dark or light
+    siteUrl: 'https://osmankiral.netlify.app/', // your website URL
+    siteLogo: '/file.svg',
+    socialBanner: '/next.png', // add social banner in the public folder
+    email: 'osman.kiral96@gmail.com', 
+    github: 'https://github.com/osmankiral',
+    linkedin: 'https://www.linkedin.com/in/osmankiral/',
+    instagram: 'https://www.instagram.com/kiralosm/',
+    dribbble: 'https://www.dribbble.com',
+    locale: 'en-US',
+  }
